87119bf3eec1a10ce33ea04d63ca4e031f79ea36
zhongyongsheng <zhongyongsheng@yy.com>
2019-01-21 16:09:35 +0800

    add custom dispatcher to test

--- protoqueue-runtime/src/main/java/net/protoqueue/ProtoQueueCoroutine.kt
+++ protoqueue-runtime/src/main/java/net/protoqueue/ProtoQueueCoroutine.kt
@@ -7,11 +7,14 @@ import kotlinx.coroutines.*
  * 使用这个方法来执行protoqueue任务，由于protoqueue请求并不会阻塞，只会异步等待服务器返回数据，因此专门指定了context=Dispatchers.Default
  * Created by zhongyongsheng on 2018/9/24.
  */
+
+internal val dispatcher = newSingleThreadContext("ProtoQueueDispatcher")
+
 fun protoQueueLaunch(start: CoroutineStart = CoroutineStart.DEFAULT,
                      block: suspend CoroutineScope.() -> Unit
-) = GlobalScope.launch (Dispatchers.Default, start, block)
+) = GlobalScope.launch (dispatcher, start, block)
 
 fun <T> protoQueueAsync(start: CoroutineStart = CoroutineStart.DEFAULT,
                     onCompletion: CompletionHandler? = null,
                     block: suspend CoroutineScope.() -> T
-) = GlobalScope.async (Dispatchers.Default, start, block)
+) = GlobalScope.async (dispatcher, start, block)
