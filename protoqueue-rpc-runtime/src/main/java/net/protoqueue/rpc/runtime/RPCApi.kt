60b03f2901e43aa37e2af1f20617533e1f09d2ed
Vincent Zhong <itvincent@gmail.com>
2019-08-15 10:42:10 +0800

    增加接口

--- protoqueue-rpc-runtime/src/main/java/net/protoqueue/rpc/runtime/RPCApi.kt
+++ protoqueue-rpc-runtime/src/main/java/net/protoqueue/rpc/runtime/RPCApi.kt
@@ -3,3 +3,44 @@ package net.protoqueue.rpc.gen
 /**
  * Created by zhongyongsheng on 2019-08-13.
  */
+object RPCApi {
+    internal var adapter: RPCAdapter? = null
+    fun initialize(_adapter: RPCAdapter) {
+        adapter = _adapter
+    }
+}
+
+/**
+ * 成功回调
+ */
+typealias RPCResponseCallback = (
+    serverName: String,
+    funcName: String, protoType: String,
+    data: ByteArray
+) -> Unit
+
+/**
+ * 错误回调
+ */
+typealias RPCErrorCallback = (sdkResCode: Int, srvResCode: Int) -> Unit
+
+/**
+ * 通知接通器
+ */
+typealias RPCNotifyReceiver = (
+    serviceName: String,
+    functionName: String,
+    data: ByteArray
+) -> Unit
+
+interface RPCAdapter {
+    fun send(
+        serverName: String, functionName: String, bytes: ByteArray, parameter: RPCParameter? = null,
+        successCallback: RPCResponseCallback,
+        errorCallback: RPCErrorCallback
+    )
+
+    fun subscribeNotify(serverName: String, functionName: String, receiver: RPCNotifyReceiver)
+}
+
+data class RPCParameter(val timeout: Long = 0L)
