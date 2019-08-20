7e8e1fc8ded2f07ccb8fd51776cc19e8ebf14de1
Vincent Zhong <itvincent@gmail.com>
2019-08-20 11:53:04 +0800

    支持泛型

--- protoqueue-rpc-gen/src/main/java/net/protoqueue/rpc/gen/struct/DataObjectStruct.kt
+++ protoqueue-rpc-gen/src/main/java/net/protoqueue/rpc/gen/struct/DataObjectStruct.kt
@@ -6,21 +6,38 @@ import kotlin.reflect.KClass
  * PB Message的转换类
  * Created by zhongyongsheng on 2019-08-20.
  */
-data class DataObjectFileStruct(/*放置转换类的文件包名*/val filePackage: String,
+class DataObjectFileStruct(/*放置转换类的文件包名*/val filePackage: String,
     /*放置转换类的文件名*/val fileName: String
 ) {
-    val objects = listOf<DataObjectStruct>()
+    val objects = mutableListOf<DataObjectStruct>()
 
     override fun toString(): String {
         return "DataObjectFileStruct(filePackage='$filePackage', fileName='$fileName', objects=$objects)"
     }
 }
 
-data class DataObjectStruct(val messageType: String) {
-    val fields = listOf<DataFieldStruct>()
+class DataObjectStruct(val messageType: String) {
+    val fields = mutableListOf<DataFieldStruct>()
     override fun toString(): String {
         return "DataObjectStruct(messageType='$messageType', fields=$fields)"
     }
 }
 
-data class DataFieldStruct(val fieldName: String, val fieldTypeClass: KClass<*>?, val fieldType: String?)
+class DataFieldStruct(val fieldName: String)
+
+open class DataFieldType(val fieldTypeClass: KClass<*>?, val fieldType: String?)
+
+class DataFieldParameterType(fieldTypeClass: KClass<*>?, fieldType: String?) :
+    DataFieldType(fieldTypeClass, fieldType) {
+    val parameterTypes = mutableListOf<DataFieldType>()
+
+    fun addParameterTypes(vararg types: DataFieldType) = apply {
+        types.forEach { parameterTypes.add(it) }
+    }
+
+    companion object {
+        fun get(fieldTypeClass: KClass<*>?, fieldType: String?, vararg types: DataFieldType): DataFieldParameterType {
+            return DataFieldParameterType(fieldTypeClass, fieldType).addParameterTypes(*types)
+        }
+    }
+}
