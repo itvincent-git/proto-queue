544ab19d0c9e08d00795d4e003837dc54c905869
Vincent Zhong <itvincent@gmail.com>
2019-08-22 11:40:29 +0800

    转类型

--- protoqueue-rpc-gen/src/main/java/net/protoqueue/rpc/gen/writer/ProtoDataObjectWriter.kt
+++ protoqueue-rpc-gen/src/main/java/net/protoqueue/rpc/gen/writer/ProtoDataObjectWriter.kt
@@ -32,6 +32,7 @@ class ProtoDataObjectWriter(private val dataObjectFileStruct: DataObjectFileStru
             .writeTo(outputDir)
     }
 
+    //创建DO外部类
     private fun createDataObjectWrapperClass(builder: FileSpec.Builder) {
         TypeSpec.classBuilder(dataObjectFileStruct.fileClassName)
             .apply {
@@ -40,6 +41,7 @@ class ProtoDataObjectWriter(private val dataObjectFileStruct: DataObjectFileStru
             }
     }
 
+    //创建多个DO类
     private fun createDataObjects(builder: TypeSpec.Builder) {
         for (dataObjectStruct in dataObjectFileStruct.objects) {
             TypeSpec.classBuilder(dataObjectStruct.genMessageTypeSimpleName)
@@ -51,6 +53,7 @@ class ProtoDataObjectWriter(private val dataObjectFileStruct: DataObjectFileStru
         }
     }
 
+    //创建字段属性
     private fun createDataObjectFields(builder: TypeSpec.Builder, dataObjectStruct: DataObjectStruct) {
         for (fieldStruct in dataObjectStruct.fields) {
             PropertySpec.builder(fieldStruct.fieldName, getFieldTypeName(fieldStruct.fieldType))
@@ -62,6 +65,7 @@ class ProtoDataObjectWriter(private val dataObjectFileStruct: DataObjectFileStru
         }
     }
 
+    //生成字段类型
     private fun getFieldTypeName(type: DataFieldType): TypeName = when (type) {
         is DataFieldParameterType -> {
             val parameterTypes = type.parameterTypes.map {
@@ -74,6 +78,7 @@ class ProtoDataObjectWriter(private val dataObjectFileStruct: DataObjectFileStru
         }
     }
 
+    //生成字段初始化值
     private fun getFieldInitializer(type: DataFieldType, propertyBuilder: PropertySpec.Builder) =
         when (type.fieldType) {
             "kotlin.collections.MutableList" -> {
@@ -84,11 +89,34 @@ class ProtoDataObjectWriter(private val dataObjectFileStruct: DataObjectFileStru
             }
         }
 
+    //创建DO转Message类方法
     private fun createConvertToMessageFunction(builder: TypeSpec.Builder, dataObjectStruct: DataObjectStruct) {
         FunSpec.builder("convertToMessage")
             .returns(dataObjectStruct.originMessageTypeClassName)
             .apply {
-
+                //val message = UserLoginRes()
+                addStatement("val message = %T()", dataObjectStruct.originMessageTypeClassName)
+                for (field in dataObjectStruct.fields) {
+                    if (field.fieldType.isOriginalType) {
+                        //message.firstLoginTime = firstLoginTime ?: 0
+                        val fieldSetStatement = when (field.fieldType.fieldType) {
+                            "kotlin.Int" -> " ?: 0"
+                            "kotlin.Boolean" -> " ?: false"
+                            "kotlin.Double" -> " ?: 0.0"
+                            "kotlin.Float" -> " ?: 0.0f"
+                            "kotlin.Long" -> " ?: 0L"
+                            //message.accountList = accountList.map { it.convertToMessage() }.toTypedArray()
+                            "kotlin.collections.MutableList" -> ".map { it.convertToMessage() }.toTypedArray()"
+                            else -> ""
+                        }
+                        addStatement("message.%L = %L%L", field.fieldName, field.fieldName, fieldSetStatement)
+                    } else {
+                        //message.header = header?.convertToMessage()
+                        addStatement("message.%L = %L?.convertToMessage()", field.fieldName, field.fieldName)
+                    }
+                }
+                //return message
+                addStatement("return message")
                 builder.addFunction(build())
             }
     }
