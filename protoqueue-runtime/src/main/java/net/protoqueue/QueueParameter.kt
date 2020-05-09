322bea0f5ccbfadbac75b440687fc9b7c7868e7b
zhongyongsheng <zhongyongsheng@yy.com>
2020-05-09 15:44:22 +0800

    format

--- protoqueue-runtime/src/main/java/net/protoqueue/QueueParameter.kt
+++ protoqueue-runtime/src/main/java/net/protoqueue/QueueParameter.kt
@@ -1,14 +1,15 @@
 package net.protoqueue
 
-
 /**
  * 创建enqueue的参数
  * Created by zhongyongsheng on 2018/6/7.
  */
-class QueueParameter<P, C> internal constructor(private var protoQueue: ProtoQueue<P, C>,
-                                                private var proto: P,
-                                                private var receiveUri: Int,
-                                                private var receiver: (P) -> Unit) {
+class QueueParameter<P, C> internal constructor(
+    private var protoQueue: ProtoQueue<P, C>,
+    private var proto: P,
+    private var receiveUri: Int,
+    private var receiver: (P) -> Unit
+) {
     internal var error: ((ProtoError) -> Unit?)? = null
     internal var timeout = DEFAULT_TIMEOUT
 
@@ -38,11 +39,11 @@ class QueueParameter<P, C> internal constructor(private var protoQueue: ProtoQue
      */
     fun enqueue(): ProtoDisposable {
         return this.protoQueue.enqueueInternal(proto,
-                receiveUri,
-                protoQueue.iGetTopSid(),
-                protoQueue.iGetSubSid(),
-                receiver,
-                this)
+            receiveUri,
+            protoQueue.iGetTopSid(),
+            protoQueue.iGetSubSid(),
+            receiver,
+            this)
     }
 
     companion object {
