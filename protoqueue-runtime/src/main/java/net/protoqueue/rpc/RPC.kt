073f825704c823a275e869a26bc46133c4a612e0
zhongyongsheng <zhongyongsheng@yy.com>
2020-05-20 10:33:59 +0800

    修改注册方法

--- protoqueue-runtime/src/main/java/net/protoqueue/rpc/RPC.kt
+++ protoqueue-runtime/src/main/java/net/protoqueue/rpc/RPC.kt
@@ -1,10 +1,17 @@
 package net.protoqueue.rpc
 
 /**
+ * RPC调用的接口
  * Created by zhongyongsheng on 2020/4/22.
  */
 interface RPC<REQ, RES> {
+    /**
+     * 发送请求并接收回复
+     */
     suspend fun request(req: REQ, parameter: RequestParameter? = null): Response<RES?>
 
-    suspend fun registerResponse(res: RES?, parameter: RequestParameter? = null)
+    /**
+     * 注册监听广播或单播
+     */
+    suspend fun registerResponse(block: (RES?, ResponseParameter?) -> Unit)
 }
--- protoqueue-runtime/src/main/java/net/protoqueue/rpc/ResponseRegister.kt
+++ protoqueue-runtime/src/main/java/net/protoqueue/rpc/ResponseRegister.kt
@@ -6,7 +6,7 @@ import net.protoqueue.ProtoQueue
  * 回包的回调注册器
  * Created by zhongyongsheng on 2020/5/11.
  */
-class ResponseRegister<PROTO, C>(private val protoqueue: ProtoQueue<PROTO, C>) {
+class ResponseRegister<PROTO, C>(private val protoQueue: ProtoQueue<PROTO, C>) {
     private val registerMap = mutableMapOf<Int, MutableList<(PROTO) -> Unit>>()
 
     fun addRegister(uri: Int, block: (PROTO) -> Unit) {
@@ -18,7 +18,7 @@ class ResponseRegister<PROTO, C>(private val protoqueue: ProtoQueue<PROTO, C>) {
     }
 
     fun onReceive(value: PROTO) {
-        val uri = protoqueue.getReceiveUri(value)
+        val uri = protoQueue.getReceiveUri(value)
         val registerList = registerMap[uri]
         if (registerList != null) {
             for (register in registerList) {
